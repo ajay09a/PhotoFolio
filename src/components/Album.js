fdfe5c6dc027ebbe9baad9cebb2b7586a52ac627
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-04-21 17:42:03 +0530

    add new album to the list

--- src/components/Album.js
+++ src/components/Album.js
@@ -4,12 +4,32 @@ import picture from '../assets/picture.png';
 const Album = () => {
     const [changebutton, setchangebutton] = useState("false");
     const [changebuttontext, setchangebuttontext] = useState("Add album")
+    const [newalbum, setnewalbum] = useState("");
     const handleChangeButton= ()=>{
         changebutton==="false"?<>{setchangebutton("true")}{setchangebuttontext("Cancel")}</>:<>{setchangebutton("false")}{setchangebuttontext("Add album")}</>;
     }
+    const createNewAlbum = (e)=>{
+        e.preventDefault();
+        setnewalbum(e.target.value);
+    }
+    const clearNewAlbum =()=>{
+        setnewalbum("");
+    }
+    const handlesubmit =(e)=>{
+        e.preventDefault();
+        console.log(newalbum);
+        setnewalbum("");
+    }
   return (
     <div className='album-list'>
-        {changebutton==="true"?<div className='add-album'><span>Create an album</span><form><input required placeholder="Album Name" /><button type="button">Clear</button><button>Create</button></form></div>:null}
+        {changebutton==="true"?
+            (<div className='add-album'>
+                <span>Create an album</span>
+                <form onSubmit={handlesubmit}>
+                    <input required placeholder="Album Name" value={newalbum} onChange={createNewAlbum} />
+                    <button type="button" onClick={clearNewAlbum}>Clear</button>
+                    <button>Create</button></form></div>
+            ):null}
         <div className='album-list-top'>
             <h3>Your albums</h3>
             <button className={changebutton} onClick={handleChangeButton}>{changebuttontext}</button>
